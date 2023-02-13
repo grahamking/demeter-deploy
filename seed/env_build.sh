23b05490c944a04e8fa98e64231ba0a2f01d0328
Mike Kot <to@myrrc.dev>
2023-02-13 21:04:23 +0300

    Add github actions yaml, smaller de/ release file

--- seed/env_build.sh
+++ seed/env_build.sh
@@ -6,4 +6,4 @@
 # Adjust target-cpu to match you server. Find it like this:
 # `gcc -march=native -Q --help=target | grep march`
 
-export RUSTFLAGS="-Ctarget-cpu=core-avx2 -Clink-args=-nostartfiles -Crelocation-model=static -Clink-args=-Wl,-n,-N,--no-dynamic-linker,--no-pie,--build-id=none,--no-eh-frame-hdr"
+export RUSTFLAGS="-Ctarget-cpu=core-avx2 -Clink-args=-nostartfiles -Crelocation-model=static -Clink-args=-Wl,-n,-N,--no-dynamic-linker,--build-id=none,--no-eh-frame-hdr"
